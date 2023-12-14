36e1e84ad8d3593778be092e45c666e4a5a35d0a
Jose Miguel Loyola <coteloyola@hotmail.com>
2023-12-14 00:57:32 -0300

    create eventoeventoevento

--- miEvento.php
+++ miEvento.php
@@ -25,7 +25,7 @@ require_once('./includes/head.php');
                         <li class="breadcrumb-item active" aria-current="page"><a class="txtDec-no" href="#">Crear evento</a></li>
                     </ol>
                 </nav>
-                <p class="headerTitle" style="margin:-13px 0px 16px 0px!important;">Crear un nuevo evento</p>
+                <p class="headerTitle" style="margin:-13px 0px 16px 0px!important;">Crear un nuevo eventoeventoeventoevento</p>
             </header>
 
             <div class="pageContent">
