c6614ac44578b785d91b399b7f648caf136d8366
Jose Miguel Loyola <coteloyola@hotmail.com>
2023-12-26 14:08:29 -0300

    event list logs remove

--- js/eventList.js
+++ js/eventList.js
@@ -22,6 +22,8 @@ async function getEvents(empresa_id){
 async function printAllProjects(){
 
     $('#allProjectTable-list tr').remove();
+
+    
     _projectsToList.forEach((evento)=>{
         let color = "";
 
