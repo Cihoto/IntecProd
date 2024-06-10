7f501837994a9349b15c087d8a65278df709af73
Jose Miguel Loyola <coteloyola@hotmail.com>
2024-06-10 19:23:38 -0400

    new test 3 page

--- inventory.php
+++ inventory.php
@@ -16,3 +16,46 @@ ob_end_flush();
 $title = "Intec - Eventos";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+require_once('./includes/head.php');
+$active = 'inventario';
+?>
+<body>
+    <?php include_once('./includes/Constantes/empresaId.php') ?>
+    <?php include_once('./includes/Constantes/rol.php') ?>
+</body>
+
+<script>
+    function getCatsFromInventory(empresa_id) {
+
+fetch('/ws/productos/getCategories.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            empresaId: empresa_id,
+            action: "getCategorias",
+        })
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        // insertProds(json)
+        let select = $('#categoriaSelect')
+        json.forEach(cat => {
+            let opt = $(select).append(new Option(capitalizeFirstLetter(cat.nombre), cat.id))
+        });
+        console.log('getCatsFromInventory', json);
+        console.log('getCatsFromInventory', json);
+        console.log('getCatsFromInventory', json);
+        console.log('getCatsFromInventory', json);
+        console.log('getCatsFromInventory', json);
+    })
+    .catch((err) => console.log(err));
+}
+</script>
+</html>
