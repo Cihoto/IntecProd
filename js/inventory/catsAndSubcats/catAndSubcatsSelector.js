56ef7f2a1569f38dd3eac1b9e56602ad75bbb0d4
Jose Miguel Loyola <coteloyola@hotmail.com>
2024-06-10 19:37:23 -0400

    ITEM INVENTARIO

--- js/inventory/catsAndSubcats/catAndSubcatsSelector.js
+++ js/inventory/catsAndSubcats/catAndSubcatsSelector.js
@@ -82,25 +82,25 @@ function GetItems() {
         })
         .catch((err) => console.log(err));
 
-    return $.ajax({
-        type: "POST",
-        url: "/ws/categoria_item/item.php",
-        data: JSON.stringify({
-            action: "getItems",
-            empresaId: EMPRESA_ID
-        }),
-        dataType: 'json',
-        success: async function (data) {
-            // let select = $('#itemSelect');
+    // return $.ajax({
+    //     type: "POST",
+    //     url: "/ws/categoria_item/item.php",
+    //     data: JSON.stringify({
+    //         action: "getItems",
+    //         empresaId: EMPRESA_ID
+    //     }),
+    //     dataType: 'json',
+    //     success: async function (data) {
+    //         // let select = $('#itemSelect');
 
-            _allSubCategoriesForProdData = data
-            // data.forEach(cat=>{
-            //     let opt  = $(select).append(new Option(capitalizeFirstLetter(cat.item), cat.id))
-            // })
-        }, error: function (response) {
-            console.log(response.responseText);
-        }
-    })
+    //         _allSubCategoriesForProdData = data
+    //         // data.forEach(cat=>{
+    //         //     let opt  = $(select).append(new Option(capitalizeFirstLetter(cat.item), cat.id))
+    //         // })
+    //     }, error: function (response) {
+    //         console.log(response.responseText);
+    //     }
+    // })
 }
 
 
